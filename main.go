ae02d6b3f9ed931202c4480a39a6f69d69612650
Slypot <sylvain.pottier@ynov.com>
2026-06-05 12:03:28 +0200

    REINITIALISATION DU PROJET

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
-init projet
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Println("Serveur lancé sur http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
