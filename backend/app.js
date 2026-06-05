9b37d6874c31642529c05d1d8da01951a9ca2009
Slypot <sylvain.pottier@ynov.com>
2026-06-05 09:50:02 +0200

    Base mise en place FRONT/BACK

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
-
 app.use(cors());
 app.use(express.json());
 
-const path = require("path");
-
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-app.use("/products", routesProduits);
-
-app.listen(port, () => {
-  console.log("Serveur lancé sur http://localhost:" + port);
+app.get("/", (req, res) => {
+  res.json({ message: "Backend opérationnel !" });
 });
+
+app.listen(3000, () => console.log("Serveur en ligne sur http://localhost:3000"));
